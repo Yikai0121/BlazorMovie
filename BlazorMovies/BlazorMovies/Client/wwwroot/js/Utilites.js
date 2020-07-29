9f2505c50472d2d9296f41ec7178cd705779e808
Yikai0121 <k2lairrol@gmail.com>
2020-07-29 17:30:54 +0800

    Finish

---
+++ BlazorMovies/BlazorMovies/Client/wwwroot/js/Utilites.js
@@ -0,0 +1,17 @@
+﻿function my_function(message) {
+    console.log("from utilities" + message);
+}
+
+//javascript 調用C#靜態方法
+function dotnetStaticInvocation() {
+    DotNet.invokeMethodAsync("BlazorMovies.Client", "GetCurrentCount")
+        //then 返回
+        .then(result => {
+            console.log("count from javascript" + result);
+        });
+}
+//javascript 調用C#實例方法
+function dotnetInstanceInvocation(dotnetHelper) {
+    dotnetHelper.invokeMethodAsync("IncrementCount");
+}
+
